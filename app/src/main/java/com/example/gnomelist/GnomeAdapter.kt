b31bc21e21fc30298569139df4deb7a076271bd1
jlguambo <jlguambo@gmail.com>
2021-08-29 01:29:59 -0500

    Add code for second antivity intent

--- app/src/main/java/com/example/gnomelist/GnomeAdapter.kt
+++ app/src/main/java/com/example/gnomelist/GnomeAdapter.kt
@@ -11,9 +11,22 @@ import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_gnome.*
 import kotlinx.android.synthetic.main.item_gnome.view.*
+import java.util.*
 import kotlin.coroutines.coroutineContext
 
 class GnomeAdapter( val gnomes: List<Brastlewark>): RecyclerView.Adapter<GnomeAdapter.GnomeHolder>(){
+
+    var arrayOfString = arrayOf("")
+    var listOfString = arrayOfString.toList()
+    var currentBrastlewark = Brastlewark(id=0,
+     name ="",
+     thumbnail = "",
+     age = 200,
+     weight = 1.0,
+     height = 2.0,
+     hair_color = "",
+    professions = listOfString ,
+    friends = listOfString);
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GnomeHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -21,6 +34,7 @@ class GnomeAdapter( val gnomes: List<Brastlewark>): RecyclerView.Adapter<GnomeAd
     }
 
     override fun onBindViewHolder(holder: GnomeHolder, position: Int) {
+        currentBrastlewark = gnomes[position]
         holder.render(gnomes[position])
     }
 
@@ -29,6 +43,7 @@ class GnomeAdapter( val gnomes: List<Brastlewark>): RecyclerView.Adapter<GnomeAd
     }
 
     inner class GnomeHolder(val view:View): RecyclerView.ViewHolder(view){
+
 
         fun  render(brastlewark: Brastlewark){
             view.tvGnome.text = brastlewark.name
@@ -46,7 +61,18 @@ class GnomeAdapter( val gnomes: List<Brastlewark>): RecyclerView.Adapter<GnomeAd
                 Toast.makeText(view.context,"You clicked on position ${position + 1}",
                 Toast.LENGTH_SHORT).show()
 
+                var listOfProfessions = emptyArray<String>()
+                for (i in 0..currentBrastlewark.professions.size - 1) {
+
+
+                    listOfProfessions[i] = currentBrastlewark.professions.get(i)
+                }
+
+
+
                 val intent = Intent(v.context, MainActivity2::class.java)
+                intent.putExtra("currentBrastlewark",currentBrastlewark.name);
+                intent.putExtra("currentBrastlewarkProfessions",listOfProfessions);
                 v.context.startActivity(intent)
 
             }
--- app/src/main/java/com/example/gnomelist/MainActivity2.kt
+++ app/src/main/java/com/example/gnomelist/MainActivity2.kt
@@ -2,10 +2,19 @@ package com.example.gnomelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+
+
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val intent = intent
+        val value1 = intent.getStringExtra("currentBrastlewarkProfessions")
+        val value2 = intent.getStringExtra("currentBrastlewark")
+        println("value1 " + value1)
+        println("value2 " + value2)
     }
 }
