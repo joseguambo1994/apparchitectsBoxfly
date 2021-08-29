26a2760956672a5d5e281bbb91711c1d3fcd2bc0
jlguambo <jlguambo@gmail.com>
2021-08-29 00:04:31 -0500

    Add data fetch from JSON

--- app/src/main/java/com/example/gnomelist/GnomeAdapter.kt
+++ app/src/main/java/com/example/gnomelist/GnomeAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.gnomelist
+import Brastlewark
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_gnome.*
 import kotlinx.android.synthetic.main.item_gnome.view.*
+import kotlin.coroutines.coroutineContext
 
-class GnomeAdapter( val gnomes: List<Gnome>): RecyclerView.Adapter<GnomeAdapter.GnomeHolder>(){
+class GnomeAdapter( val gnomes: List<Brastlewark>): RecyclerView.Adapter<GnomeAdapter.GnomeHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GnomeHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -24,10 +27,15 @@ class GnomeAdapter( val gnomes: List<Gnome>): RecyclerView.Adapter<GnomeAdapter.
 
     class GnomeHolder(val view:View): RecyclerView.ViewHolder(view){
 
-        fun  render(gnome: Gnome){
-            view.tvGnome.text = gnome.name
-            Picasso.get().load(gnome.imageUrl)
-                .resize(100,100).into(view.ivGnome)
+        fun  render(brastlewark: Brastlewark){
+            view.tvGnome.text = brastlewark.name
+//            Picasso.get().load(gnome.imageUrl)
+//                .resize(100,100).into(view.ivGnome)
+            Glide.with(view)
+                .load(brastlewark.thumbnail)
+                .into(view.ivGnome)
+
+
         }
     }
 
--- app/src/main/java/com/example/gnomelist/MainActivity.kt
+++ app/src/main/java/com/example/gnomelist/MainActivity.kt
@@ -1,46 +1,104 @@
 package com.example.gnomelist
 
+import Brastlewark
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import java.util.Collections.emptyList
-import java.util.Collections.list
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    var gnomes = arrayOf(
-        Gnome("name1","http://www.publicdomainpictures.net/pictures/10000/nahled/thinking-monkey-11282237747K8xB.jpg"),
-        Gnome("name2", "http://i.imgur.com/tGbaZCY.jpg"),
-        Gnome("name2", "https://imgur.com/a/48DpmEb"),
-        Gnome("name2", "http://i.imgur.com/DvpvklR.png"),
-        Gnome("white-hem","http://www.publicdomainpictures.net/pictures/120000/nahled/white-hen.jpg"),
-        Gnome("white-hen https","https://www.publicdomainpictures.net/pictures/120000/nahled/white-hen.jpg"),
-        Gnome("name2", "http://i.imgur.com/DvpvklR.png"),
-        Gnome("name2", "http://www.publicdomainpictures.net/pictures/10000/nahled/1-1275919724d1Oh.jpg"),
-        Gnome("name2", "http://i.imgur.com/DvpvklR.png")
-    )
+    val listOfBrastlewark = LinkedList<Brastlewark>()
+//    var gnomes = arrayOf(
+//        Gnome("name1","https://www.publicdomainpictures.net/pictures/10000/nahled/thinking-monkey-11282237747K8xB.jpg"),
+//        Gnome("name2", "https://i.imgur.com/tGbaZCY.jpg"),
+//        Gnome("name2", "https://imgur.com/a/48DpmEb"),
+//        Gnome("name2", "https://i.imgur.com/DvpvklR.png"),
+//        Gnome("white-hem","https://www.publicdomainpictures.net/pictures/120000/nahled/white-hen.jpg"),
+//        Gnome("white-hen https","https://www.publicdomainpictures.net/pictures/120000/nahled/white-hen.jpg"),
+//        Gnome("name2", "https://i.imgur.com/DvpvklR.png"),
+//        Gnome("name2", "https://www.publicdomainpictures.net/pictures/10000/nahled/1-1275919724d1Oh.jpg"),
+//        Gnome("name2", "https://i.imgur.com/DvpvklR.png")
+//    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       //pupolateList()
-        println("gnomes 0 : "+gnomes[0])
-        println("gnomes 1 : "+gnomes[1])
         setContentView(R.layout.activity_main)
 
+        getUsers()
         initRecycler()
+        println("listOfBrastlewark " +listOfBrastlewark)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        println("onResume listOfBrastlewark " +listOfBrastlewark)
+        initRecycler()
 
     }
 
 
     fun initRecycler() {
         rvSuperHero.layoutManager = LinearLayoutManager(this)
-        val gnomesList: List<Gnome> = gnomes.toList()
-        println("list + " + gnomesList)
-        val adapter = GnomeAdapter(gnomesList);
+     //   val gnomesList: List<Gnome> = gnomes.toList()
+        println("list + " + listOfBrastlewark)
+        getUsers()
+        val adapter = GnomeAdapter(listOfBrastlewark.toList());
         rvSuperHero.adapter=adapter
+    }
+
+    fun getUsers() {
+        val queue = Volley.newRequestQueue(this)
+        val url: String = "https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json"
+
+        val  urlTest: String ="https://jsonplaceholder.typicode.com/posts/1"
+
+        val stringReq = StringRequest(Request.Method.GET, "https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json",
+            Response.Listener<String> { response ->
+
+                val jsonObject = JSONObject(response)
+                val listOfArray = jsonObject.getJSONArray("Brastlewark")
+                println("length "+ listOfArray.length())
+
+                println("listOfArray[2] +" +listOfArray[2])
+
+                println("listOfArray" + listOfArray)
+
+                val oneBrastlewarkFromListOfArray = listOfArray[2].toString()
+                val gson = Gson()
+                var oneBrastlewarkFromListOfArrayToJson = gson.fromJson(
+                    oneBrastlewarkFromListOfArray, Brastlewark::class.java
+                )
+                println("oneBrastlewarkFromListOfArrayToJson " + oneBrastlewarkFromListOfArrayToJson)
+
+                for (i in 1..(listOfArray.length()-1)) {
+                    val oneBrastlewarkFromListOfArray = listOfArray[i].toString()
+                    val gson = Gson()
+                    var oneBrastlewarkFromListOfArrayToJson = gson.fromJson(
+                        oneBrastlewarkFromListOfArray, Brastlewark::class.java
+                    )
+                    listOfBrastlewark.add(oneBrastlewarkFromListOfArrayToJson)
+                }
+
+                println("mutableList.size + "+listOfBrastlewark.size);
+                println("First two items")
+                println("mutableList[0]")
+                println(listOfBrastlewark.get(0))
+                println("mutableList[1]")
+                println(listOfBrastlewark.get(1))
+                println("mutableList[2]")
+                println(listOfBrastlewark.get(2))
+
+            },
+            Response.ErrorListener { print("Can't download")})
+        queue.add(stringReq)
     }
 
 
